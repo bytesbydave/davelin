6c302e7b8b85f9445551147c549dd04bdcfbee40
Dave <dave.lin21@gmail.com>
2017-05-23 19:55:41 +0000

    redone back page

--- SendGrid-API/send-email.php
+++ SendGrid-API/send-email.php
@@ -34,6 +34,8 @@ $response = $sg->client->mail()->send()->post($mail);
 <!--Print the response-->
 <pre>
     <?php
-    var_dump($response);
+    $link_address1 = '../index.php';
+    echo "Your message has been sent";
+    echo "<a href='".$link_address1."' class='btn btn-primary'>Back</a>";
     ?>
 </pre>
