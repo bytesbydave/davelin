652450e747c1b20b8b99b3fafb90aa164cb0ecc3
Dave <dave.lin21@gmail.com>
2017-05-23 05:38:32 +0000

    create personal site with email integration

---
+++ SendGrid-API/send-email.php
@@ -0,0 +1,36 @@
+<?php
+/*SendGrid Library*/
+require_once ('vendor/autoload.php');
+
+/*Post Data*/
+$name = $_POST['name'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+
+/*Content*/
+$from = new SendGrid\Email("David Lin", "david.lin21@gmail.com");
+$subject = "SUBJECT";
+$to = new SendGrid\Email("Dave", "dave.lin21@gmail.com");
+$content = new SendGrid\Content("text/html", "
+  Email : {$email}<br>
+  Name : {$name}<br>
+  Message : {$message}
+  "
+
+  );
+
+/*Send the mail*/
+$mail = new SendGrid\Mail($from, $subject, $to, $content);
+$apiKey = ('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');
+$sg = new \SendGrid($apiKey);
+
+/*Response*/
+$response = $sg->client->mail()->send()->post($mail);
+?>
+
+<!--Print the response-->
+<pre>
+    <?php
+    var_dump($response);
+    ?>
+</pre>
---
+++ index.php
@@ -0,0 +1,271 @@
+<!DOCTYPE HTML>
+<!--
+	Hyperspace by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+	<head>
+		<title>Dave Lin</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<link rel="stylesheet" href="https://cdn.rawgit.com/konpa/devicon/master/devicon.min.css">
+		<!--[if lte IE 9]><link rel="stylesheet" href="assets/css/ie9.css" /><![endif]-->
+		<!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
+	</head>
+	<link rel="shortcut icon" href="images/hire.png">
+	<body>
+
+		<!-- Sidebar -->
+			<section id="sidebar">
+				<div class="inner">
+					<nav>
+						<ul>
+							<li><a href="#intro">Welcome</a></li>
+							<li><a href="#one">Projects</a></li>
+							<li><a href="#two">Experience</a></li>
+							<li><a href="#three">Contact</a></li>
+						</ul>
+					</nav>
+				</div>
+			</section>
+
+		<!-- Wrapper -->
+			<div id="wrapper">
+
+				<!-- Intro -->
+					<section id="intro" class="wrapper style1 fullscreen fade-up">
+						<div class="inner">
+							<h1>Dave Lin</h1>
+							<p>New York City Based Web Developer and Theater Producer</p>
+							<ul class="actions">
+								<li><a href="#one" class="button scrolly">Learn more</a></li>
+							</ul>
+						</div>
+					</section>
+
+				<!-- One -->
+					<section id="one" class="wrapper style2 spotlights">
+						
+						<section>
+							<a href="https://www.rokawellness.com/" target="_blank" class="image"><img src="images/pic01.jpg" alt="" data-position="center center" /></a>
+							<div class="content">
+								<div class="inner">
+									<h2>Roka Wellness</h2>
+									<p>Roka Wellness is a mental health app that connects users with various mental health professionals.</p>
+									<ul class="actions">
+										<li><a href="https://www.rokawellness.com/" target="_blank" class="button">Check Out The App</a></li>
+									</ul>
+								</div>
+							</div>
+						</section>
+						<section>
+							<a href="https://davemintrecipes.herokuapp.com/" target="_blank" class="image"><img src="images/pic02.jpg" alt="" data-position="top center" /></a>
+							<div class="content">
+								<div class="inner">
+									<h2>MyRecipes App</h2>
+									<p>MyRecipes is a cooking web-based app where chefs can create their own recipes and view other recipes by other chefs. This solo project was accomplished using Ruby on Rails.</p>
+									<ul class="actions">
+										<li><a href="https://davemintrecipes.herokuapp.com/" target="_blank" class="button">Check Out The App</a></li>
+									</ul>
+								</div>
+							</div>
+						</section>
+						<section>
+							<a href="http://jamsessiondbc.herokuapp.com/" target="_blank" class="image"><img src="images/pic03.jpg" alt="" data-position="25% 25%" /></a>
+							<div class="content">
+								<div class="inner">
+									<h2>Jam Session</h2>
+									<p>A web-based music repository where amateur musicians can showcase their talents and collaborate with other musicians on projects. This four-person project was accomplished in one week and uses Rails backend and React front-end.</p>
+									<ul class="actions">
+										<li><a href="http://jamsessiondbc.herokuapp.com/" target="_blank" class="button">Check Out The App</a></li>
+									</ul>
+								</div>
+							</div>
+						</section>
+					</section>
+
+				<!-- Two -->
+					<section id="two" class="wrapper style3 fade-up">
+						<div class="inner">
+							<h2>Work History</h2>
+							<p>Over the past few years, I have worked at a variety of cool startups in the industries of construction, entertainment, real-estate, and healthcare. </p>
+							<div class="features">
+								<section>
+									<span class="icon major fa-plus-square"></span>
+									<h3><a href="https://www.rokawellness.com/" target="_blank">Roka Wellness</a></h3>
+									<h5>Web Developer 2017-Present</h5>
+									<p>Roka Wellness is an early stage startup that connects its users to mental health professionals, such as life coach, yoga instructors, or psycologists.</p>
+								</section>
+								<section>
+									<span class="icon major fa-building"></span>
+									<h3><a href="https://www.urban3d.co/" target="_blank">Urban3D</a></h3>
+									<h5>Web/Business Developer 2016-2017</h5>
+									<p>Urban3D is a software solution that enables precast manufacturers to reduce costs by tracking concrete throughout the project lifecycle.</p>
+								</section>
+								<section>
+									<span class="icon major fa-ticket"></span>
+									<h3><a href="https://www.indigogrey.nyc/" target="_blank">Indigo Grey</a></h3>
+									<h5>Business Development 2015-Present</h5>
+									<p>Indigo Grey is an immersive theater production that places the audience at the center of the action in set in a futuristic dystopian sixth borough of New York City. </p>
+								</section>
+								<section>
+									<span class="icon major fa-file-archive-o"></span>
+									<h3><a href="https://www.greenclosings.com/" target="_blank">Green Closings</a></h3>
+									<h5>Business Development 2016-Present</h5>
+									<p>GreenClosings is a transaction management platform designed to optimize the document management process of submitting applications for co-op and condominium ownership properties</p>
+								</section>
+								<!--<section>-->
+								<!--	<span class="icon major fa-chain"></span>-->
+								<!--	<h3>Coderhouse</h3>-->
+								<!--	<p>Phasellus convallis elit id ullam corper amet et pulvinar. Duis aliquam turpis mauris, sed ultricies erat dapibus.</p>-->
+								<!--</section>-->
+								<!--<section>-->
+								<!--	<span class="icon major fa-diamond"></span>-->
+								<!--	<h3>Aliquam urna dapibus</h3>-->
+								<!--	<p>Phasellus convallis elit id ullam corper amet et pulvinar. Duis aliquam turpis mauris, sed ultricies erat dapibus.</p>-->
+								<!--</section>-->
+							</div>
+							<!--<ul class="actions">-->
+							<!--	<li><a href="#" class="button">Learn more</a></li>-->
+							<!--</ul>-->
+						</div>
+					</section>
+
+				<!-- Three -->
+					<section id="three" class="wrapper style1 fade-up">
+						<div class="inner">
+							<h2>Get in touch</h2>
+							<p></p>
+							<div class="split style1">
+								<section>
+									<div class="form-holder">
+            <!--Form-->
+									
+									<form method="post" action="SendGrid-API/send-email.php">
+										<div class="field half first">
+											<label for="name">Name</label>
+											<input type="text" name="name" id="name" />
+										</div>
+										<div class="field half">
+											<label for="email">Email</label>
+											<input type="text" name="email" id="email" />
+										</div>
+										<div class="field">
+											<label for="message">Message</label>
+											<textarea name="message" id="message" rows="5"></textarea>
+										</div>
+										<ul class="actions">
+											<button type="submit" class="btn btn-primary">Submit</button>
+											<!--<li><a href="" class="button submit">Send Message</a></li>-->
+											<!--<li><input type="submit" name="submit" id="submit" value="Send Message" /></li>-->
+
+										</ul>
+									</form>
+								</section>
+								<section>
+									<ul class="contact">
+										<h3>Developer Tools</h3>
+										
+							            
+							              <i class="devicon-html5-plain"></i>
+							              <span class="list">HTML</span>
+							              <br>
+							           
+							              <i class="devicon-css3-plain"></i>
+							              <span class="list">CSS</span>
+							              <br>
+							           
+							              <i class="devicon-javascript-plain"></i>
+							              <span class="list">Javascript</span>
+							              <br>
+
+							              <i class="devicon-ruby-plain"></i>
+							              <span class="list">Ruby</span>
+							              <br>
+
+							              <i class="devicon-rails-plain"></i>
+							              <span class="list">Ruby on Rails</span>
+							              <br>
+
+							              <i class="devicon-jquery-plain"></i>
+							              <span class="list">JQuery</span>
+							              <br>
+
+							              <i class="devicon-react-original"></i>
+							              <span class="list">React.js</span>
+							              <br>
+
+							              <i class="devicon-bootstrap-plain"></i>
+							              <span class="list">Bootstrap</span>
+							              <br>
+
+							              <i class="devicon-git-plain"></i>
+							              <span class="list">Git</span>
+							              <br>
+
+							              <i class="devicon-amazonwebservices-original"></i>
+							              <span class="list">Amazon AWS</span>
+							              <br>
+
+							              <i class="devicon-heroku-original"></i>
+							              <span class="list">Heroku</span>
+							              <br>
+							              <br>
+
+          
+										<!--<li>-->
+										<!--	<h3>Address</h3>-->
+										<!--	<span>12345 Somewhere Road #654<br />-->
+										<!--	Nashville, TN 00000-0000<br />-->
+										<!--	USA</span>-->
+										<!--</li>-->
+										<!--<li>-->
+										<!--	<h3>Email</h3>-->
+										<!--	<a href="#">user@untitled.tld</a>-->
+										<!--</li>-->
+										<!--<li>-->
+										<!--	<h3>Phone</h3>-->
+										<!--	<span>(000) 000-0000</span>-->
+										<!--</li>-->
+										<!--<li>-->
+										
+											<h3>Social</h3>
+											<ul class="icons">
+												<li><a href="https://twitter.com/djLin21" target="_blank" class="fa-twitter"><span class="label">Twitter</span></a></li>
+												<li><a href="https://www.facebook.com/david.lin.3133" target="_blank" class="fa-facebook"><span class="label">Facebook</span></a></li>
+												<li><a href="https://github.com/dvdlin214" target="_blank" class="fa-github"><span class="label">GitHub</span></a></li>
+												<!--<li><a href="" class="fa-instagram"><span class="label">Instagram</span></a></li>-->
+												<li><a href="https://www.linkedin.com/in/dvdlin214" target="_blank" class="fa-linkedin"><span class="label">LinkedIn</span></a></li>
+											</ul>
+										</li>
+									</ul>
+								</section>
+							</div>
+						</div>
+					</section>
+
+			</div>
+
+		<!-- Footer -->
+			<footer id="footer" class="wrapper style1-alt">
+				<div class="inner">
+					<ul class="menu">
+						<li>&copy; Untitled. All rights reserved.</li><li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
+					</ul>
+				</div>
+			</footer>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.scrollex.min.js"></script>
+			<script src="assets/js/jquery.scrolly.min.js"></script>
+			<script src="assets/js/skel.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<!--[if lte IE 8]><script src="assets/js/ie/respond.min.js"></script><![endif]-->
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
