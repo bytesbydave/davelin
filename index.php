e69bef82795fa52bcba436abae251f679d764277
Dave <dave.lin21@gmail.com>
2019-02-20 21:27:10 +0000

    change headline

--- index.php
+++ index.php
@@ -39,7 +39,7 @@
 					<section id="intro" class="wrapper style1 fullscreen fade-up">
 						<div class="inner">
 							<h1>Dave Lin</h1>
-							<p>New York City Based Web Developer</p>
+							<p>New York City Based Software Engineer</p>
 							<ul class="actions">
 								<li><a href="#one" class="button scrolly">Learn more</a></li>
 							</ul>
